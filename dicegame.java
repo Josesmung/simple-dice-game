4739f8563d02b9319b12dc93ec27ffdef2395ea6
Moses Jung <mojung99@uw.edu>
2020-05-20 14:22:52 -0700

    add prob for players

--- dicegame.java
+++ dicegame.java
@@ -19,7 +19,7 @@ public class dicegame {
             int roll = r.nextInt(6) + 1;
             
             //updates player turn
-            if (pIndex == 0) { //player 1 turn
+            if (pIndex == 0) { //player 1 turn (if roll is bigger than 3 then add to score, else sub from enemy player)
                pIndex++;
                if (roll > 3) {
                   pScores[0] += roll;
@@ -31,7 +31,7 @@ public class dicegame {
                   }
                } 
             } 
-            else { //player 2 turn
+            else { //player 2 turn (if roll is bigger than 5 then add to score, else sub from enemy player)
                pIndex = 0; 
                if (roll > 5) {
                   pScores[1] += roll;
@@ -44,9 +44,21 @@ public class dicegame {
                } 
             }
          }
-         System.out.print(pIndex);
+         //System.out.println(pIndex);
+         //tracks wins
+         if (pIndex == 1) {
+            wins[1]++;
+         } else {
+            wins[0]++;
+         }
          pScores = new int[2]; //resets scores
       }
       
+      System.out.println(Arrays.toString(wins));
+      //System.out.println(wins[0]);
+      double p1 = new Double(wins[0]) / nGames;
+      System.out.println("Player 1 won: " + p1);
+      double p2 = new Double(wins[1]) / nGames;
+      System.out.println("Player 2 won: " + p2);
    }
 }
